3a4c6517772c947bb2dffad4cd92efe2f9f0fae2
Nicolas Comet <nicolas.comet@gmail.com>
2023-12-03 17:01:07 +0100

    day2

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,51 @@
+data class Draw(
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0,
+)
+
+typealias Game = Draw
+
+fun List<Game>.sumIdsCompliantTo(game: Game): Int =
+    mapIndexed { i, g ->
+        if (g.compliesTo(game)) {
+            i + 1
+        } else {
+            0
+        }
+    }.sum()
+
+private fun Game.compliesTo(game: Game): Boolean =
+    red <= game.red && green <= game.green && blue <= game.blue
+
+fun Iterable<Game>.maxOfAllGames(): Game =
+    reduce { game, nextGame ->
+        game.copy(
+            red = maxOf(game.red, nextGame.red),
+            green = maxOf(game.green, nextGame.green),
+            blue = maxOf(game.blue, nextGame.blue),
+        )
+    }
+
+fun String.toMaxGame(): Game {
+    val withoutGame = split(": ").drop(1).first()
+    val draws = withoutGame.split("; ")
+    return draws
+        .map { it.toDraw() }
+        .maxOfAllGames()
+}
+
+fun Game.power(): Int = red * green * blue
+
+fun String.toDraw(): Draw = split(", ")
+    .flatMap { it.split(" ") }
+    .chunked(2)
+    .map { it[0] to it[1] }
+    .fold(Draw()) { draw, pair ->
+        when (pair.second) {
+            "red" -> draw.copy(red = pair.first.toInt())
+            "blue" -> draw.copy(blue = pair.first.toInt())
+            "green" -> draw.copy(green = pair.first.toInt())
+            else -> throw IllegalArgumentException()
+        }
+    }
---
+++ src/test/kotlin/Day2Test.kt
@@ -0,0 +1,82 @@
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import kotlin.streams.asSequence
+
+class Day2Test {
+    @Test
+    fun `toDraw should convert a Draw`() {
+        "5 blue, 4 red, 13 green".toDraw() shouldBeEqual Draw(red = 4, green = 13, blue = 5)
+    }
+
+    @Test
+    fun `toMaxGame should maxBy colors and return the Game`() {
+        val maxGame = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+            .toMaxGame()
+
+        maxGame shouldBeEqual Game(red = 20, green = 13, blue = 6)
+    }
+
+    @Test
+    fun `Should solve maxOfAllGames`() {
+        val maxGame = object {}.javaClass.classLoader
+            .getResourceAsStream("day2/sample1.txt")!!
+            .bufferedReader().lines()
+            .asSequence()
+            .map { it.toMaxGame() }
+            .toList()
+            .maxOfAllGames()
+
+        maxGame shouldBeEqual Game(red = 20, green = 13, blue = 15)
+    }
+
+    @Test
+    fun `Should get sum of possible Ids given a Game objective on sample`() {
+        val allGames = object {}.javaClass.classLoader
+            .getResourceAsStream("day2/sample1.txt")!!
+            .bufferedReader().lines()
+            .asSequence()
+            .map { it.toMaxGame() }
+            .toList()
+
+        allGames.sumIdsCompliantTo(Game(red = 12, green = 13, blue = 14)) shouldBe 8
+    }
+
+    @Test
+    fun `Should get sum of possible Ids given a Game objective on input`() {
+        val allGames = object {}.javaClass.classLoader
+            .getResourceAsStream("day2/input.txt")!!
+            .bufferedReader().lines()
+            .asSequence()
+            .map { it.toMaxGame() }
+            .toList()
+
+        allGames.sumIdsCompliantTo(Game(red = 12, green = 13, blue = 14)) shouldBe 2_439
+    }
+
+    @Test
+    fun `Should get power of all games on sample`() {
+        val allMinGames = object {}.javaClass.classLoader
+            .getResourceAsStream("day2/sample1.txt")!!
+            .bufferedReader().lines()
+            .asSequence()
+            .map { it.toMaxGame() }
+            .toList()
+
+
+        allMinGames.sumOf { it.power() } shouldBe 2_286
+    }
+
+    @Test
+    fun `Should get power of all games on input`() {
+        val allMinGames = object {}.javaClass.classLoader
+            .getResourceAsStream("day2/input.txt")!!
+            .bufferedReader().lines()
+            .asSequence()
+            .map { it.toMaxGame() }
+            .toList()
+
+
+        allMinGames.sumOf { it.power() } shouldBe 63_711
+    }
+}
